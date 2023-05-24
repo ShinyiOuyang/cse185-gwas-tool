f6c334a1728c7056e782d5dfded1a662a589e120
ShinyiOuyang <103228741+ShinyiOuyang@users.noreply.github.com>
2023-05-23 18:41:37 -0700

    made working setup.py file

---
+++ ourgwas/ourgwas.py
@@ -0,0 +1,13 @@
+"""
+Script to perform whole genome association analysis with linear regression
+"""
+import argparse 
+from ourgwas import __version__
+import os
+
+def main():
+    print("Hello world")
+
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,32 @@
+# Used template from https://github.com/gymreklab/cse185-demo-project/tree/main
+import os
+from setuptools import setup, find_packages
+
+# version-keeping code based on pybedtools
+curdir = os.path.abspath(os.path.dirname(__file__))
+MAJ = 0
+MIN = 0
+REV = 0
+VERSION = '%d.%d.%d' % (MAJ, MIN, REV)
+with open(os.path.join(curdir, 'ourgwas/version.py'), 'w') as fout:
+    fout.write(
+        "\n".join(["",
+                   "# THIS FILE IS GENERATED FROM SETUP.PY",
+                   "version = '{version}'",
+                   "__version__ = version"]).format(version=VERSION)
+    )
+
+
+setup(
+    name='ourgwas',
+    version=VERSION,
+    description='CSE185 Our GWAS',
+    author='Riya Kalra, Shinyi Ouyang, Tyler Yang',
+    author_email='email@ucsd.edu',
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "ourgwas=ourgwas.ourgwas:main"
+        ],
+    },
+)
