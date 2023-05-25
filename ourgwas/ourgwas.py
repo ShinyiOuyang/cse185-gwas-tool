ea367fa18ffbc3860521d81d5e7bfd8172e76731
ShinyiOuyang <103228741+ShinyiOuyang@users.noreply.github.com>
2023-05-25 14:32:29 -0700

    add example files and temporary parser arguments

--- ourgwas/ourgwas.py
+++ ourgwas/ourgwas.py
@@ -4,9 +4,30 @@ Script to perform whole genome association analysis with linear regression
 import argparse 
 from ourgwas import __version__
 import os
+import cyvcf2 as vcf
 
+# Sets up script argument parsing
+parser = argparse.ArgumentParser()
+
+# Input
+parser.add_argument("--pheno", type=argparse.FileType('r'))
+# https://www.cog-genomics.org/plink/1.9/input#pheno
+
+parser.add_argument("--vcf", type=argparse.FileType('r'))
+
+#Options
+parser.add_argument("--pca", type=int)
+parser.add_argument("--maf", type=float)
+
+# Output
+parser.add_argument("--out", type=argparse.FileType('w'))
+
+args = parser.parse_args()
+
+# TODO
 def main():
-    print("Hello world")
+    #for variant in vcf(args.vcf):
+    print("Hello World")
 
 
 if __name__ == "__main__":
